7c57c19fca3dfa82b99ade2c3c6453e4e469c4c7
PragmaticCrow <pragmaticcrowoficial@gmail.com>
2023-07-10 21:33:29 -0600

    añadiendo anya.py, anyaSay.py, anya_say.py y crow.py

---
+++ anya_say.py
@@ -0,0 +1,55 @@
+import sys
+def anya_say(mensaje):
+    if len(sys.argv) > 1:
+        mensaje = ' '.join(sys.argv[1:])
+        
+    else:
+        print("Usage: python crowsay.py [message]")
+
+    mensaje = sys.argv[1:]
+    caracteres = len(mensaje)
+    longitud = caracteres // 52 + 1
+
+
+    print(" " + "_" * 54)
+    print("/" + " " * 54 + "\\")
+    if caracteres <= 51:
+        print("| " + mensaje + (" " * (53 - caracteres)) + "|")
+    elif caracteres >= 52:
+        for i in range(1, longitud):
+            inicial = 1
+            multiplicar = i - 1
+            if i > 1:
+                inicial = 0
+            print("| " + mensaje[multiplicar * 52 + inicial: i * 52] + " |" + "\n" + "|" + " " * 54 + "|")
+            if i + 1 == longitud:
+                local = len(mensaje[i * 52: 999999999999999999999999999999999999999999999]) 
+                print("| " + mensaje[i * 52: 999999999999999999999999999999999999999999999] + " " * (53 - local) + "|")
+        
+    print("\\ " + "_" * 53 + "/")
+    print("                 \  \\  ")
+    print("                  \  \\  ")
+    print("                   \  \\  ")
+    print("                        ⢲⢄")
+    print("                       ⠀⡆")
+    print("        ⠀⠀⠀⠀⠀⠀⢀⠄⠂⢉⠤⠐⠋⠈⠡⡈⠉⠐⠠⡀          ")
+    print("        ⠀⠀⠀⢀⡀⢠⣤⠔⠁⢀⠀⠀⠀⠀⠀⠀⠀⠈⢢⠀⠀⠈⠱⡤⣤⠄⣀")
+    print("        ⠀⠀⠰⠁⠀⣰⣿⠃⠀⢠⠃⢸⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠈⢞⣦⡀⠈⡇")
+    print("          ⠀⢇⣠⡿⠁⠀⢀⡃⠀⣈⠀⠀⠀⠀⢰⡀⠀⠀⠀⠀⢢⠰⠀⠀⢺⣧⢰")
+    print("         ⠀⠀⠈⣿⠁⡘⠀⡌⡇⠀⡿⠸⠀⠀⠀⠈⡕⡄⠀⠐⡀⠈⠀⢃⠀⠀⠾⠇")
+    print("         ⠀⠀⠀⠇⡇⠃⢠⠀⠶⡀⡇⢃⠡⡀⠀⠀⠡⠈⢂⡀⢁⠀⡁⠸⠀⡆⠘⡀")
+    print("         ⠀⠀⠸⠀⢸⠀⠘⡜⠀⣑⢴⣀⠑⠯⡂⠄⣀⣣⢀⣈⢺⡜⢣⠀⡆⡇⠀⢣")
+    print("         ⠀⠀⠇⠀⢸⠀⡗⣰⡿⡻⠿⡳⡅⠀⠀⠀⠀⠈⡵⠿⠿⡻⣷⡡⡇⡇⠀⢸⣇")
+    print("         ⠀⢰⠀⠀⡆⡄⣧⡏⠸⢠⢲⢸⠁⠀⠀⠀⠀⠐⢙⢰⠂⢡⠘⣇⡇⠃⠀⠀⢹⡄")
+    print("          ⠟⠀⠀⢰⢁⡇⠇⠰⣀⢁⡜⠀⠀⠀⠀⠀⠀⠘⣀⣁⠌⠀⠃⠰⠀⠀⠀⠈⠰")
+    print("         ⡘⠀⠀⠀⠀⢊⣤⠀⠀⠤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠄⠀⢸⠃⠀⠀⠀⠀⠀⠃")
+    print("        ⢠⠁⢀⠀⠀⠀⠈⢿⡀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⢀⠏⠀⠀⠀⠀⠀⠀⠸")
+    print("        ⠘⠸⠘⡀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠁⠀⠃⠀⠀⠀⠀⢀⠎⠀⠀⠀⠀⠀⢠⠀⠀⡇")
+    print("        ⠀⠇⢆⢃⠀⠀⠀⠀⠀⡏⢲⢤⢀⡀⠀⠀⠀⠀⠀⢀⣠⠄⡚⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀")
+    print("        ⢰⠈⢌⢎⢆⠀⠀⠀⠀⠁⣌⠆⡰⡁⠉⠉⠀⠉⠁⡱⡘⡼⠇⠀⠀⠀⠀⢀⢬⠃⢠⠀⡆")
+    print("        ⠀⢢⠀⠑⢵⣧⡀⠀⠀⡿⠳⠂⠉⠀⠀⠀⠀⠀⠀⠀⠁⢺⡀⠀⠀⢀⢠⣮⠃⢀⠆⡰⠀")
+    print("        ⠀⠀⠑⠄⣀⠙⡭⠢⢀⡀⠀⠁⠄⣀⣀⠀⢀⣀⣀⣀⡠⠂⢃⡀⠔⠱⡞⢁⠄⣁⠔⠁⠀")
+    print("        ⠀⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠉⠁⠀⠀⠀⠀")
+    print("        ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀")
+    
+    anya_say(mensaje)
